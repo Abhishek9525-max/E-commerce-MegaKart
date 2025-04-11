9fca91e23218d369bb79533ad9657a9ce9732310
Abhishek <abhisheksaxena9525@gmail.com>
2025-04-11 12:57:53 +0530

    Starting

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import UserLayout from './components/Layout/UserLayout'
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path= '/' element={<UserLayout />}>
+        {/* user layout */}
+        {/* try */}
+      </Route>
+      <Route>
+        {/* admin layout */}
+      </Route>
+
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
