e86caed35ebe6bc99316ca3e0f07a1823445a903
Abhishek <abhisheksaxena9525@gmail.com>
2025-04-11 13:27:00 +0530

    Search Button functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,6 @@ function App() {
     <Routes>
       <Route path= '/' element={<UserLayout />}>
         {/* user layout */}
-        {/* try */}
       </Route>
       <Route>
         {/* admin layout */}
