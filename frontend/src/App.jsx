231fd3d5442591df5c2cf7f93ce8a3ee09611122
Abhishek <abhisheksaxena9525@gmail.com>
2025-04-13 12:17:10 +0530

    Hero Section Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import UserLayout from './components/Layout/UserLayout'
+import Home from './pages/Home'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path= '/' element={<UserLayout />}>
         {/* user layout */}
+        <Route index element={<Home/>}/>
       </Route>
       <Route>
         {/* admin layout */}
