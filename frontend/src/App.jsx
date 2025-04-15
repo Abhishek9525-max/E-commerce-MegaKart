f864f9ab7f272335ccb0f1637be796f770234fd1
Abhishek <abhisheksaxena9525@gmail.com>
2025-04-15 17:03:11 +0530

    registration page complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import UserLayout from './components/Layout/UserLayout'
 import Home from './pages/Home'
 import {Toaster} from 'sonner'
 import Login from './pages/Login'
+import Register from './pages/Register'
 function App() {
   return (
     <BrowserRouter >
@@ -13,6 +14,7 @@ function App() {
         {/* user layout */}
         <Route index element={<Home/>}/>
         <Route path='login' element={<Login/>}/>
+        <Route path='register' element={<Register/>}/>
       </Route>
       <Route>
         {/* admin layout */}
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import login from '../assets/login.webp'
+import register from '../assets/register.webp'
 
-const Login = () => {
+const Register = () => {
+  const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-
+  
   const handleSubmit = (e)=>{
     e.preventDefault();
-    console.log('user Login',{name,email,password})
+    console.log('user registered',{name,email,password})
   }
 
   return (
@@ -22,6 +23,16 @@ const Login = () => {
           <p className='text-center mb-6'>
             Enter your username and password to Login
           </p>
+          <div className='mb-4'>
+            <label className='block text-sm font-semibold mb-2'>Name</label>
+            <input
+              type="email"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className='w-full p-2 border rounded'
+              placeholder='Enter you Name'
+            />
+          </div>
           <div className='mb-4'>
             <label className='block text-sm font-semibold mb-2'>Email</label>
             <input
@@ -42,29 +53,30 @@ const Login = () => {
               placeholder='Enter your password'
             />
           </div>
-          <button type='submit'
+          <button 
           onClick={handleSubmit}
+          type='submit'
             className='w-full bg-black text-white p-2 rounded-lg font-semibold hover:bg-gray-800 transition'
           >
-            Sign In
+            Sign Up
           </button>
           <p className='mt-6 text-center text-sm'>
             Don't have an account? {" "}
-            <Link to='/register' className='text-blue-500'>Register</Link>
+            <Link to='/register' className='text-blue-500'>Login</Link>
           </p>
         </form>
       </div>
       <div className='hidden md:block w-1/2 bg-gray-800'>
-      <div className='h-full flex flex-col justify-center items-center'>
-    <img 
-    src={login} 
-    alt="Login to Account" 
-    className='h-[750px] w-full object-cover'
-    />
-      </div>
+        <div className='h-full flex flex-col justify-center items-center'>
+          <img
+            src={register}
+            alt="Login to Account"
+            className='h-[750px] w-full object-cover'
+          />
+        </div>
       </div>
     </div>
   )
 }
 
-export default Login
+export default Register
