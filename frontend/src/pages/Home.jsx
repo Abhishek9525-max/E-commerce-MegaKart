1768544412aa34cb9eec326febb1e5342488bd82
Abhishek <abhisheksaxena9525@gmail.com>
2025-04-15 13:29:36 +0530

    feature collection complete

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,55 +4,56 @@ import GenderCollectionSection from '../components/Products/GenderCollectionSect
 import NewArrivals from '../components/Products/NewArrivals'
 import ProductDetails from '../components/Products/ProductDetails'
 import ProductGrid from '../components/Products/ProductGrid'
+import FeaturedCollection from '../components/Products/FeaturedCollection'
 
 
 const placeholderProducts = [
   {
-    id:1,
-    name:'Product 1',
-    price:100,
-    images:[{url: 'https://picsum.photos/500/500?random=1'}]
+    id: 1,
+    name: 'Product 1',
+    price: 100,
+    images: [{ url: 'https://picsum.photos/500/500?random=1' }]
   },
   {
-    id:2,
-    name:'Product 2',
-    price:100,
-    images:[{url: 'https://picsum.photos/500/500?random=2'}]
+    id: 2,
+    name: 'Product 2',
+    price: 100,
+    images: [{ url: 'https://picsum.photos/500/500?random=2' }]
   },
   {
-    id:3,
-    name:'Product 3',
-    price:100,
-    images:[{url: 'https://picsum.photos/500/500?random=3'}]
+    id: 3,
+    name: 'Product 3',
+    price: 100,
+    images: [{ url: 'https://picsum.photos/500/500?random=3' }]
   },
   {
-    id:4,
-    name:'Product 4',
-    price:100,
-    images:[{url: 'https://picsum.photos/500/500?random=4'}]
+    id: 4,
+    name: 'Product 4',
+    price: 100,
+    images: [{ url: 'https://picsum.photos/500/500?random=4' }]
   }, {
-    id:5,
-    name:'Product 1',
-    price:100,
-    images:[{url: 'https://picsum.photos/500/500?random=1'}]
+    id: 5,
+    name: 'Product 1',
+    price: 100,
+    images: [{ url: 'https://picsum.photos/500/500?random=1' }]
   },
   {
-    id:6,
-    name:'Product 2',
-    price:100,
-    images:[{url: 'https://picsum.photos/500/500?random=2'}]
+    id: 6,
+    name: 'Product 2',
+    price: 100,
+    images: [{ url: 'https://picsum.photos/500/500?random=2' }]
   },
   {
-    id:7,
-    name:'Product 3',
-    price:100,
-    images:[{url: 'https://picsum.photos/500/500?random=3'}]
+    id: 7,
+    name: 'Product 3',
+    price: 100,
+    images: [{ url: 'https://picsum.photos/500/500?random=3' }]
   },
   {
-    id:8,
-    name:'Product 4',
-    price:100,
-    images:[{url: 'https://picsum.photos/500/500?random=4'}]
+    id: 8,
+    name: 'Product 4',
+    price: 100,
+    images: [{ url: 'https://picsum.photos/500/500?random=4' }]
   }
 ]
 
@@ -60,24 +61,25 @@ const placeholderProducts = [
 const Home = () => {
   return (
     <div>
-      <Hero/>
-      <GenderCollectionSection/>
-      <NewArrivals/>
+      <Hero />
+      <GenderCollectionSection />
+      <NewArrivals />
 
       {/* Best Seller  */}
       <h2 className='text-3xl text-center font-bold mb-4'>
         Best Seller
       </h2>
-      <ProductDetails/>
+      <ProductDetails />
 
 
       <div className='container mx-auto'>
         <h2 className='text-3xl  text-center font-bold mb-4'>
           Top Wear  for Women
         </h2>
-        <ProductGrid products={placeholderProducts}/>
+        <ProductGrid products={placeholderProducts} />
       </div>
-      </div>
+      <FeaturedCollection/>
+    </div>
   )
 }
 
