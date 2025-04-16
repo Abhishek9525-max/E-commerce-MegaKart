f009f2aef0c70a1338431ec574230d85fadbbd1c
Abhishek <abhisheksaxena9525@gmail.com>
2025-04-16 13:20:04 +0530

    PayPal Payment Integrated

--- frontend/src/components/Cart/PayPalButton.jsx
+++ frontend/src/components/Cart/PayPalButton.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js";
 
-const PayPalButton = () => {
+const PayPalButton = ({ amount, onSuccess, onError }) => {
   return (
-    <div>PayPalButton</div>
-  )
-}
+    <PayPalScriptProvider
+      options={{
+        "client-id":
+          "AQX2rwJq5sdHdo7w1l4HtOdu0b9CkIU6SBBPmHkhthBpEzOpMvF6epx_vjc8igQR15BieE4ypoEoQwqv",
+      }}
+    >
+      <PayPalButtons
+        style={{ layout: "vertical" }}
+        createOrder={(data, action) => {
+          return action.order.create({
+            purchase_units: [{ amount: { value: amount } }],
+          });
+        }}
+        onApprove={(data, actions) => {
+          return actions.order.capture().then(onSuccess);
+        }}
+        onError={onError}
+      />
+    </PayPalScriptProvider>
+  );
+};
 
-export default PayPalButton
+export default PayPalButton;
