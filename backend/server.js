3d85112d058e1f9a820924fd953b39f81be0ef57
Abhishek <abhisheksaxena9525@gmail.com>
2025-04-20 11:03:11 +0530

    create product complete

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const cors = require('cors')
-const dotenv = require("dotenv")
-const connectDB = require('./config/db')
-const userRoutes = require('./routes/userRoutes')
-
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 app.use(express.json());
@@ -11,19 +11,19 @@ app.use(cors());
 
 dotenv.config();
 
-const PORT =process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 //Connect to MongoDB
 connectDB();
 
-app.get('/', (req, res)=>{
-  res.send('WELCOME TO MEGAKART API')
+app.get("/", (req, res) => {
+  res.send("WELCOME TO MEGAKART API");
 });
 
-
 //API Routes
-app.use('/api/users', userRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
 
-app.listen(PORT,()=>{
-  console.log(`Server is running on http://localhost:${PORT}`);  
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
