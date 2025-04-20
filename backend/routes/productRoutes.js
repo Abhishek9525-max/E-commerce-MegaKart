78e15cd57125c04a0c786361a6bdb85d961a3270
Abhishek <abhisheksaxena9525@gmail.com>
2025-04-20 13:46:03 +0530

    find One flter complete

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -242,4 +242,21 @@ router.get("/", async (req, res) => {
   }
 });
 
+// @route GET /api/products/:id
+// @desc Get a single product by Id
+// @access Public
+router.get("/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: "Product Not Found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
