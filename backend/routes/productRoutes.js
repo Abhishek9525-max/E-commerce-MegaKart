83b34dbb10d145b200ca015ac41ca20d1c8d592f
Abhishek <abhisheksaxena9525@gmail.com>
2025-04-20 14:05:21 +0530

    new arrival filter complete

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -248,12 +248,26 @@ router.get("/", async (req, res) => {
 
 router.get("/best-seller", async (req, res) => {
   try {
-    const bestSeller = await Product.findOne().sort({rating: -1});
-    if(bestSeller){
+    const bestSeller = await Product.findOne().sort({ rating: -1 });
+    if (bestSeller) {
       res.json(bestSeller);
-    }else{
-      res.status(404).json({message: "No best seller found"})
+    } else {
+      res.status(404).json({ message: "No best seller found" });
     }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error");
+  }
+});
+
+// @route GET /api/products/new-arrivals
+// @desc Retrieve latest 8 products- Creation date
+// @access Public
+router.get("/new-arrivals", async (req, res) => {
+  try {
+    //Fetch latest 8 products
+    const newArrivals = await Product.find().sort({ createdAt: -1 }).limit(8);
+    res.json(newArrivals);
   } catch (error) {
     console.error(error);
     res.status(500).send("Server error");
