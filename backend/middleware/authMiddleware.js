9f575dddc3224e845cd42ed32ce1382901c09e41
Abhishek <abhisheksaxena9525@gmail.com>
2025-04-21 10:52:39 +0530

    checkout routes completed

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,7 +3,7 @@ const User = require("../models/User");
 
 //Middleware to protect routes
 
-const protect = async (req, resizeBy, next) => {
+const protect = async (req, res, next) => {
   let token;
 
   if (
@@ -17,7 +17,7 @@ const protect = async (req, resizeBy, next) => {
       next();
     } catch (error) {
       console.error("Token verification failed", error);
-      resizeBy.status(401).json({ message: "Not authorized, token failed" });
+      res.status(401).json({ message: "Not authorized, token failed" });
     }
   } else {
     res.status(401).json({ message: "Not authorized, no token provided" });
@@ -26,12 +26,12 @@ const protect = async (req, resizeBy, next) => {
 
 //Middleware to check if the user is an admin
 
-const admin = (req,res,next)=>{
-  if(req.user && req.user.role === 'admin'){
+const admin = (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
     next();
-  }else{
-    res.status(403).json({message: 'Not authorized as an admin'});
+  } else {
+    res.status(403).json({ message: "Not authorized as an admin" });
   }
-}
+};
 
-module.exports = { protect,admin };
+module.exports = { protect, admin };
