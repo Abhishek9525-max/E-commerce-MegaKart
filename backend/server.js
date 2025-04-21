e82a36403a27387454bc37a4c1a17972ad55310d
Abhishek <abhisheksaxena9525@gmail.com>
2025-04-21 21:14:29 +0530

    admin product route complete

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const orderRoutes = require("./routes/orderRoutes");
 const uploadRoutes = require("./routes/uploadRoutes");
 const subscribeRoutes = require("./routes/subscribeRoute");
 const adminRoutes = require("./routes/adminRoutes");
+const productAdminRoutes = require("./routes/productAdminRoutes");
 
 const app = express();
 app.use(express.json());
@@ -39,6 +40,7 @@ app.use("/api", subscribeRoutes);
 
 
 app.use("/api/admin/users", adminRoutes);
+app.use("/api/admin/products", productAdminRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
