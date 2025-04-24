419001ecb011195b75a1cdaf827894330209c05a
Abhishek <abhisheksaxena9525@gmail.com>
2025-04-24 15:36:48 +0530

    shop icon

--- frontend/src/components/Layout/Hero.jsx
+++ frontend/src/components/Layout/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
               Explore our vaction-ready outfits with fast worldwide shipping.
             </p>
             <Link 
-            to='#' 
+            to='/collection/all?gender=Men' 
             className='bg-white text-gray-950 px-6 py-2 rounded-sm text-lg '
             >
               Shop Now
